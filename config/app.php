c484d4815b6954036706ce431dcad57e7aca3038
Jclavo <jclavotafur@gmail.com>
2020-05-26 16:46:41 -0300

    add spatie for providers

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        //Spatie is for permissions
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Package Service Providers...
