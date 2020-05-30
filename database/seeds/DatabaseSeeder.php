d95946a4f5e6bee4897b8bb2e1fcdb4e4e3383f7
Jclavo <jclavotafur@gmail.com>
2020-05-30 17:51:22 -0300

    add project seeeder to Main seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(SpatieSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(ProjectSeeder::class);
     }
 }
