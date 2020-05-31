8eef9f4b6ac77a398b9d786b43525568252df25c
Jclavo <jclavotafur@gmail.com>
2020-05-31 16:34:20 -0300

    migration add resource_id to table permissions

---
+++ database/migrations/2020_05_31_191046_add_resources_to_permissions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddResourcesToPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->unsignedBigInteger('resource_id')->nullable(true);
+            $table->foreign('resource_id')->references('id')->on('resources')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            //
+        });
+    }
+}
