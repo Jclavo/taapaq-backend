f0eefe494921b43e0b1ad9336acbe7c211ddc806
Jclavo <jclavotafur@gmail.com>
2020-05-31 12:02:27 -0300

    create files to Resource Module

---
+++ database/factories/ResourceFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Resource;
+use App\Models\Page;
+use Faker\Generator as Faker;
+
+$factory->define(Resource::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'page_id' => Page::all()->random()->id,
+    ];
+});
