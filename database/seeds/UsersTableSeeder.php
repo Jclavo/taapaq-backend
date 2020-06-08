cd04d0cd81960e886457682c2ded024e829069c7
Jclavo <jclavotafur@gmail.com>
2020-06-07 22:05:05 -0300

    comment seeder from user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,15 +12,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = [
-            '1111',
-            '2222',
-            '3333',
-            '4444'
-        ];
+        // $users = [
+        //     '1111',
+        //     '2222',
+        //     '3333',
+        //     '4444'
+        // ];
 
-        foreach ($users as $user) {
-            factory(User::class)->create(['login' => $user]);
-        }  
+        // foreach ($users as $user) {
+        //     factory(User::class)->create(['login' => $user]);
+        // }  
     }
 }
