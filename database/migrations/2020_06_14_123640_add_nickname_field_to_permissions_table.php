dc66baeeaa5d1213770b8a07c1abba694c370a17
Jclavo <jclavotafur@gmail.com>
2020-06-14 09:59:05 -0300

    add field nickname to permissions table

---
+++ database/migrations/2020_06_14_123640_add_nickname_field_to_permissions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNicknameFieldToPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->string('nickname',45);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            //
+        });
+    }
+}
