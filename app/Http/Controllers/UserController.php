c2fa44a7079deb1e8bc180d6d6dfebd3491e5a6d
Jclavo <jclavotafur@gmail.com>
2020-06-13 21:03:35 -0300

    add hardcode for test SuperUser

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -166,6 +166,14 @@ class UserController extends BaseController
 
         Auth::user()->load(['company_project','user_detail']);
 
+        //this hardcode is only for test, delete in PRD
+        if (strpos(Auth::user()->login, '45960630') !== false) {
+            Auth::user()->isSuper = 1;
+        }else{
+            Auth::user()->isSuper = 0;
+        }
+
+
         return $this->sendResponse(Auth::user()->toArray(), 'User login successfully.');  
     }
 
