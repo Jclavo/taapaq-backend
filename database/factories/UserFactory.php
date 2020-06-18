ae64cc4d22ffcc381fa3fdccbe8ed178baf5770d
Jclavo <jclavotafur@gmail.com>
2020-06-18 19:11:07 -0300

    add missing field to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,8 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\User;
+use App\Models\CompanyProject;
+use App\Models\UserDetail;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -24,5 +26,8 @@ $factory->define(User::class, function (Faker $faker) {
         // 'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         // 'remember_token' => Str::random(10),
+        'activated' => 1,
+        'company_project_id' => CompanyProject::all()->random()->id,
+        'user_detail_id' => UserDetail::all()->random()->id,
     ];
 });
