527d89957cd4cbd85580c2696542f41e4702432c
Jclavo <jclavotafur@gmail.com>
2020-07-05 16:12:28 -0300

    add method update in UserDetailControlller

--- app/Http/Controllers/UserDetailController.php
+++ app/Http/Controllers/UserDetailController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule; 
 
 class UserDetailController extends BaseController
 {
@@ -53,7 +54,7 @@ class UserDetailController extends BaseController
             'email' => ['nullable','email','unique:user_details'],
             'name' => 'required|max:45',
             'lastname' => 'required|max:45',
-            'phone' => ['required', 'max:45'],
+            'phone' => ['required', 'max:45','unique:user_details'],
             'address' => 'required|max:100'
         ]);
       
@@ -104,9 +105,33 @@ class UserDetailController extends BaseController
      * @param  \App\UserDetail  $userDetail
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, UserDetail $userDetail)
+    public function update(int $id, Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'identification' => ['required', 'numeric', 'min:8', Rule::unique('user_details')->ignore($id)],
+            'email' => ['nullable','email',Rule::unique('user_details')->ignore($id)],
+            'name' => 'required|max:45',
+            'lastname' => 'required|max:45',
+            'phone' => ['required', 'max:45', Rule::unique('user_details')->ignore($id)],
+            'address' => 'required|max:100'
+        ]);
+      
+        if ($validator->fails()) {
+            return $this->sendError($validator->errors()->first());
+        }
+
+        $user = UserDetail::findOrFail($id);
+        
+        $user->identification = $request->identification;
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->lastname = $request->lastname;
+        $user->phone = $request->phone;
+        $user->address = $request->address;
+
+        $user->save();
+        
+        return $this->sendResponse($user->toArray(), 'User updated successfully.');  
     }
 
     /**
