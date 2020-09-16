9d7ca450509afd820266e476f4d1aa0c499d02a1
Jclavo <jclavotafur@gmail.com>
2020-09-16 19:07:48 -0300

    add module system models

---
+++ app/Http/Controllers/SystemModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SystemModel;
+use Illuminate\Http\Request;
+
+class SystemModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SystemModel  $systemModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SystemModel $systemModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SystemModel  $systemModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SystemModel $systemModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SystemModel  $systemModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SystemModel $systemModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SystemModel  $systemModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SystemModel $systemModel)
+    {
+        //
+    }
+}
---
+++ database/factories/SystemModelFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\SystemModel;
+use Faker\Generator as Faker;
+
+$factory->define(SystemModel::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
