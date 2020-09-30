c34a9ec8da96abffc2112d5391e3a3995bb54965
Jclavo <jclavotafur@gmail.com>
2020-09-30 14:06:53 -0300

    add mass assign/factory for SystemModel

--- app/Models/SystemModel.php
+++ app/Models/SystemModel.php
@@ -6,7 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class SystemModel extends Model
 {
-    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name','project_id'
+    ];
+
     /**
      * RELATIONSHIPS
      */
--- database/factories/SystemModelFactory.php
+++ database/factories/SystemModelFactory.php
@@ -3,10 +3,12 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\SystemModel;
+use App\Models\Project;
 use Faker\Generator as Faker;
 
 $factory->define(SystemModel::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'project_id' => Project::all()->random()->id,
     ];
 });
