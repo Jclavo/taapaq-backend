87368e5f4f3d4c5a18712eb87fb9b77ddeea58ec
Jclavo <jclavotafur@gmail.com>
2020-11-01 17:56:16 -0300

    add values for Module/Resource seeder

--- database/seeds/ModuleSeeder.php
+++ database/seeds/ModuleSeeder.php
@@ -13,6 +13,16 @@ class ModuleSeeder extends Seeder
      */
     public function run()
     {
+        ModuleUtil::createMassiveFromProjectCode('A1',self::getForTaapaq());
+    }
+
+
+    /**
+     * VALUES SECTION
+     */
+
+    static function getForTaapaq(){
+
         // $labelTapaaq = new Module();
         // $labelTapaaq->name = 'Taapaq';
         // $labelTapaaq->url = null;
@@ -31,51 +41,61 @@ class ModuleSeeder extends Seeder
         $moduleCompany->name = 'Companies';
         $moduleCompany->url = '/company-list';
         $moduleCompany->icon = null;
+        $moduleCompany->resources = ResourceSeeder::getResourceCommons();
 
         $moduleProject = new Module();
         $moduleProject->name = 'Projects';
         $moduleProject->url = '/project-list';
         $moduleProject->icon = null;
+        $moduleProject->resources = ResourceSeeder::getForProjectModule();
 
         $moduleModule = new Module();
         $moduleModule->name = 'Modules';
         $moduleModule->url = '/module-list';
         $moduleModule->icon = null;
+        $moduleModule->resources = ResourceSeeder::getResourceCommons();
 
         $moduleResource = new Module();
         $moduleResource->name = 'Resources';
         $moduleResource->url = '/resource-list';
         $moduleResource->icon = null;
+        $moduleResource->resources = ResourceSeeder::getResourceCommons();
 
         $moduleRole = new Module();
         $moduleRole->name = 'Roles';
         $moduleRole->url = '/role-list';
         $moduleRole->icon = null;
+        $moduleRole->resources = ResourceSeeder::getForRoleModule();
 
         $moduleUser = new Module();
         $moduleUser->name = 'Users';
         $moduleUser->url = '/user-list';
         $moduleUser->icon = null;
+        $moduleUser->resources = ResourceSeeder::getForUserModule();
 
         $moduleUniversalPerson = new Module();
         $moduleUniversalPerson->name = 'Universal Persons';
         $moduleUniversalPerson->url = '/user-detail-list';
         $moduleUniversalPerson->icon = null;
+        $moduleUniversalPerson->resources = ResourceSeeder::getResourceCommons();
 
         $modulePermission = new Module();
         $modulePermission->name = 'Permissions';
         $modulePermission->url = '/permission-list';
         $modulePermission->icon = null;
+        $modulePermission->resources = ResourceSeeder::getResourceCommons();
 
         $moduleModel = new Module();
         $moduleModel->name = 'Models';
         $moduleModel->url = '/model-list';
         $moduleModel->icon = null;
+        $moduleModel->resources = ResourceSeeder::getResourceCommons();
 
         $moduleTranslation = new Module();
         $moduleTranslation->name = 'Translations';
         $moduleTranslation->url = 'translation-list';
         $moduleTranslation->icon = null;
+        $moduleTranslation->resources = ResourceSeeder::getResourceCommons();
 
         $labelTapaaq->children = [ $moduleCompany,
                                    $moduleProject,
@@ -91,8 +111,7 @@ class ModuleSeeder extends Seeder
 
         $arrayTaapaq = [$labelTapaaq];
 
-        ModuleUtil::createMassiveFromProjectCode('A1',$arrayTaapaq);
-                    
+        return $arrayTaapaq;
     }
  
 }
--- database/seeds/ResourceSeeder.php
+++ database/seeds/ResourceSeeder.php
@@ -15,4 +15,53 @@ class ResourceSeeder extends Seeder
     {
         ResourceUtil::createFromProjectCodeModule('A1','Companies','read');
     }
+
+
+    /**
+     * VALUES SECTION
+    */
+
+    static function getResourceCommons(){
+        return [
+                'create',
+                'update',
+                'read',
+                'delete',
+                'pagination',
+                'show',
+        ]; 
+    }
+
+    static function getForProjectModule(){
+
+        $forProject = [
+                'assign-company',
+                'remove-company',
+            ];
+            
+        return array_merge(self::getResourceCommons(), $forProject);
+    }
+
+    static function getForRoleModule(){
+
+        $forRole = [
+                'give-permission',
+                'revoke-permission',
+            ];
+            
+        return array_merge(self::getResourceCommons(), $forRole);
+    }
+
+    static function getForUserModule(){
+
+        $forRole = [
+                'activated-status',
+                'assign-role',
+                'remove-role',
+            ];
+            
+        return array_merge(self::getResourceCommons(), $forRole);
+    }
+
+
 }
