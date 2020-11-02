de0acf016b9f8ff968ca44917d68e246894e2086
Jclavo <jclavotafur@gmail.com>
2020-11-02 19:27:14 -0300

    add function to create in CompanyUtil

--- app/Utils/CompanyUtil.php
+++ app/Utils/CompanyUtil.php
@@ -3,7 +3,6 @@
 namespace App\Utils;
 
 use App\Models\Company;
-
 use App\Utils\UniversalPersonUtil;
 
 class CompanyUtil
@@ -14,5 +13,15 @@ class CompanyUtil
         
         return Company::where('universal_person_id', $universalPersonCompany->id)->firstOrFail();
 
+    }
+
+    static function createFromIdentification($identification, $countryCode){
+        $universalPerson = UniversalPersonUtil::getFromIdentification($identification);
+        return self::createCore($universalPerson->id,$countryCode);
+    }
+
+    static function createCore($universal_person_id, $countryCode){
+
+        return Company::updateOrCreate(['universal_person_id' => $universal_person_id, 'country_code' => $countryCode]);
     }
 }
