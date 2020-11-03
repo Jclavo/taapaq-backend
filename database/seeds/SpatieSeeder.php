69b3505301f160a5d861cabe1b518b46ab8f0005
Jclavo <jclavotafur@gmail.com>
2020-11-03 18:37:52 -0300

    set value assign to common roles in seeder

--- database/seeds/SpatieSeeder.php
+++ database/seeds/SpatieSeeder.php
@@ -12,11 +12,8 @@ class SpatieSeeder extends Seeder
     public function run()
     { 
         foreach (self::getCommons() as $role){
-            $assign = false;
-            if($role == 'admin'){
-                $assign = true;
-            }
-            RoleUtil::createFromCompanyProjectCode(env('COMPANY_CLEIVOR_IDENTIFICATION'), env('PROJECT_TAAPAQ_CODE'),$role, $assign);
+            RoleUtil::createFromCompanyProjectCode(env('COMPANY_CLEIVOR_IDENTIFICATION'), env('PROJECT_TAAPAQ_CODE'),
+                                                   $role->name, $role->assign);
         }
     }
 
@@ -26,8 +23,9 @@ class SpatieSeeder extends Seeder
 
     static function getCommons(){
         return [
-                'admin',
-                'user',
+            (object) array('name' => 'admin', 'assign' => true),
+            (object) array('name' => 'user', 'assign' => false),
+            (object) array('name' => 'client', 'assign' => false)
         ]; 
     }
 }
