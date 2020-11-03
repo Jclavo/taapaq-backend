8ebb02779d2eb3106be29a7351566dd5b14f4841
Jclavo <jclavotafur@gmail.com>
2020-11-03 17:59:15 -0300

    create function to assign company

--- app/Utils/ProjectUtil.php
+++ app/Utils/ProjectUtil.php
@@ -34,221 +34,272 @@ class ProjectUtil
         //get project
         $project = Project::findOrFail($project_id);
 
-        //Create relationship between Project and Company
-        // $project->companies()->syncWithoutDetaching($company);
+        //get relationship between Project and Company
         $companyProject = $project->companies()->where('company_id', $company->id)->firstOrFail();
        
         return $companyProject->pivot->id;
     }
 
-    /***
-     * Create Project and all its connections
-     */
-    static function createProjectAndIts(string $projectName){
+    static function createCompanyProjectIDFromCode($companyIdentification, $projectCode){
 
-        DB::transaction(function () use($projectName) {
+        $company = CompanyUtil::getFromIdentification($companyIdentification);
+        $project = ProjectUtil::getFromCode($projectCode);
+        
+        return self::getCompanyProjectID($company->id,$project->id);
+    }
 
-            $resources = array(
-                'Create',
-                'Read',
-                'Update',
-                'Delete',
-                'Show',
-                'Pagination'
-            );
+    static function createCompanyProjectID($company_id, $project_id){
 
-            $resourcesRoles = $resources;
-            $resourcesUsers = $resources;
+        //get company 
+        $company = Company::findOrFail($company_id);
 
-            array_push($resourcesUsers,'activated status','assign role','remove role');
-            array_push($resourcesRoles,'give permission','revoke permission');
-            
-            
-            $modules = [
-                (object) array('name' => 'Roles', 'url' => '/role-list', 'activated' => true,
-                            'resources' => $resourcesRoles),
-                
-                (object) array('name' => 'Users', 'url' => '/user-list', 'activated' => true, 
-                            'resources' => $resourcesUsers),
-                
-                (object) array('name' => 'Users Masters', 'url' => '/user-detail-list', 'activated' => true, 'resources' => $resources),
-                
-                (object) array('name' => 'Permissions', 'url' => '/permission-list', 'activated' => false, 'resources' => $resources),
-            ];
+        //get project
+        $project = Project::findOrFail($project_id);
 
-            $roles = [
-                'admin',
-            ];
-            
-            
-            //Create a project
-            $newProject = factory(Project::class)->create(['name' => $projectName]);
+        //Create relationship between Project and Company
+        $project->companies()->syncWithoutDetaching($company);
+        $companyProject = $project->companies()->where('company_id', $company->id)->firstOrFail();
+       
+        return $companyProject->pivot->id;
+    }
 
-            //Create Roles
-            foreach ($roles as $role) {
+    static function assignCompanyFromCode($companyIdentification, $projectCode){
 
-                $adminRole = Role::create(['name' => $role . '/' . $newProject->id , 'project_id' => $newProject->id,
-                                    'nickname' => $role]);
+        $company = CompanyUtil::getFromIdentification($companyIdentification);
+        $project = ProjectUtil::getFromCode($projectCode);
+        
+        return self::assignCompany($company->id,$project->id);
+    }
 
-            }
+    static function assignCompany($company_id,$project_id){
 
-            //Create modules for the project
-            foreach ($modules as $module) {
+        $company = Company::findOrFail($company_id);
+        $project = Project::findOrFail($project_id);
+        
+        $project->companies()->syncWithoutDetaching($company); // add many to many relationship
 
-                $newModule = $newProject->modules()->save(
-                    factory(Module::class)->make(['name' => $module->name,
-                                            'url' => $module->url, 
-                                            'visibled' =>  $module->activated])
-                );
-
-                foreach ($module->resources as $resource) {
-
-                    $newResource = $newModule->resources()->save(
-                        factory(Resource::class)->make(['name' => $resource])
-                    );
-
-                    $permissionNickname = strtolower($newResource->module->name . '/' . str_replace(" ","-", $newResource->name));
-                    $permissionName = $permissionNickname . '#' . $newResource->module->project_id;
-
-                    $newPermission = Permission::create(['name' => $permissionName,
-                                                        'resource_id' => $newResource->id,
-                                                        'nickname' => $permissionNickname]);
-
-                    $adminRole->givePermissionTo($newPermission->id);
-
-                }
-            }
-
-            return $newProject;
-        });
+        return $project;
     }
 
 
-    /***
-     * Create Project and all its connections
-     */
-    static function createSuperProjectAndIts(string $projectName){
 
-        DB::transaction(function () use($projectName) {
 
-            $resources = array(
-                'Create',
-                'Read',
-                'Update',
-                'Delete',
-                'Show',
-                // 'Pagination'
-            );
 
-            $resourcesRoles = $resources;
-            $resourcesUsers = $resources;
-            $resourcesProjects = $resources;
 
-            array_push($resourcesUsers,'activated status','assign role','remove role');
-            array_push($resourcesRoles,'give permission','revoke permission');
-            array_push($resourcesProjects,'assign company','remove company');
+
+
+
+
+
+
+    // /***
+    //  * Create Project and all its connections
+    //  */
+    // static function createProjectAndIts(string $projectName){
+
+    //     DB::transaction(function () use($projectName) {
+
+    //         $resources = array(
+    //             'Create',
+    //             'Read',
+    //             'Update',
+    //             'Delete',
+    //             'Show',
+    //             'Pagination'
+    //         );
+
+    //         $resourcesRoles = $resources;
+    //         $resourcesUsers = $resources;
+
+    //         array_push($resourcesUsers,'activated status','assign role','remove role');
+    //         array_push($resourcesRoles,'give permission','revoke permission');
             
             
-            $modules = [
-
-                (object) array('name' => 'Companies', 'url' => '/company-list', 'activated' => true,
-                            'resources' => $resources),
-
-                (object) array('name' => 'Projects', 'url' => '/project-list', 'activated' => true,
-                            'resources' => $resourcesProjects),
+    //         $modules = [
+    //             (object) array('name' => 'Roles', 'url' => '/role-list', 'activated' => true,
+    //                         'resources' => $resourcesRoles),
                 
-                (object) array('name' => 'Modules', 'url' => '/module-list', 'activated' => true,
-                                'resources' => $resources),
-
-                (object) array('name' => 'Resources', 'url' => '/resource-list', 'activated' => false,
-                                'resources' => $resources),
-
-                (object) array('name' => 'Roles', 'url' => '/role-list', 'activated' => true,
-                            'resources' => $resourcesRoles),
+    //             (object) array('name' => 'Users', 'url' => '/user-list', 'activated' => true, 
+    //                         'resources' => $resourcesUsers),
                 
-                (object) array('name' => 'Users', 'url' => '/user-list', 'activated' => true, 
-                            'resources' => $resourcesUsers),
+    //             (object) array('name' => 'Users Masters', 'url' => '/user-detail-list', 'activated' => true, 'resources' => $resources),
                 
-                (object) array('name' => 'Users Masters', 'url' => '/user-detail-list', 'activated' => true,
-                            'resources' => $resources),
+    //             (object) array('name' => 'Permissions', 'url' => '/permission-list', 'activated' => false, 'resources' => $resources),
+    //         ];
+
+    //         $roles = [
+    //             'admin',
+    //         ];
+            
+            
+    //         //Create a project
+    //         $newProject = factory(Project::class)->create(['name' => $projectName]);
+
+    //         //Create Roles
+    //         foreach ($roles as $role) {
+
+    //             $adminRole = Role::create(['name' => $role . '/' . $newProject->id , 'project_id' => $newProject->id,
+    //                                 'nickname' => $role]);
+
+    //         }
+
+    //         //Create modules for the project
+    //         foreach ($modules as $module) {
+
+    //             $newModule = $newProject->modules()->save(
+    //                 factory(Module::class)->make(['name' => $module->name,
+    //                                         'url' => $module->url, 
+    //                                         'visibled' =>  $module->activated])
+    //             );
+
+    //             foreach ($module->resources as $resource) {
+
+    //                 $newResource = $newModule->resources()->save(
+    //                     factory(Resource::class)->make(['name' => $resource])
+    //                 );
+
+    //                 $permissionNickname = strtolower($newResource->module->name . '/' . str_replace(" ","-", $newResource->name));
+    //                 $permissionName = $permissionNickname . '#' . $newResource->module->project_id;
+
+    //                 $newPermission = Permission::create(['name' => $permissionName,
+    //                                                     'resource_id' => $newResource->id,
+    //                                                     'nickname' => $permissionNickname]);
+
+    //                 $adminRole->givePermissionTo($newPermission->id);
+
+    //             }
+    //         }
+
+    //         return $newProject;
+    //     });
+    // }
+
+
+    // /***
+    //  * Create Project and all its connections
+    //  */
+    // static function createSuperProjectAndIts(string $projectName){
+
+    //     DB::transaction(function () use($projectName) {
+
+    //         $resources = array(
+    //             'Create',
+    //             'Read',
+    //             'Update',
+    //             'Delete',
+    //             'Show',
+    //             // 'Pagination'
+    //         );
+
+    //         $resourcesRoles = $resources;
+    //         $resourcesUsers = $resources;
+    //         $resourcesProjects = $resources;
+
+    //         array_push($resourcesUsers,'activated status','assign role','remove role');
+    //         array_push($resourcesRoles,'give permission','revoke permission');
+    //         array_push($resourcesProjects,'assign company','remove company');
+            
+            
+    //         $modules = [
+
+    //             (object) array('name' => 'Companies', 'url' => '/company-list', 'activated' => true,
+    //                         'resources' => $resources),
+
+    //             (object) array('name' => 'Projects', 'url' => '/project-list', 'activated' => true,
+    //                         'resources' => $resourcesProjects),
                 
-                (object) array('name' => 'Permissions', 'url' => '/permission-list', 'activated' => false,
-                            'resources' => $resources),
-            ];
+    //             (object) array('name' => 'Modules', 'url' => '/module-list', 'activated' => true,
+    //                             'resources' => $resources),
 
-            $roles = [
-                'admin',
-            ];
+    //             (object) array('name' => 'Resources', 'url' => '/resource-list', 'activated' => false,
+    //                             'resources' => $resources),
 
-            $company = 'Cleivor SAC';
-            $guard = 'api';
+    //             (object) array('name' => 'Roles', 'url' => '/role-list', 'activated' => true,
+    //                         'resources' => $resourcesRoles),
+                
+    //             (object) array('name' => 'Users', 'url' => '/user-list', 'activated' => true, 
+    //                         'resources' => $resourcesUsers),
+                
+    //             (object) array('name' => 'Users Masters', 'url' => '/user-detail-list', 'activated' => true,
+    //                         'resources' => $resources),
+                
+    //             (object) array('name' => 'Permissions', 'url' => '/permission-list', 'activated' => false,
+    //                         'resources' => $resources),
+    //         ];
+
+    //         $roles = [
+    //             'admin',
+    //         ];
+
+    //         $company = 'Cleivor SAC';
+    //         $guard = 'api';
 
 
             
             
-            //Create a project
-            $newProject = factory(Project::class)->create(['name' => $projectName]);
+    //         //Create a project
+    //         $newProject = factory(Project::class)->create(['name' => $projectName]);
 
-            //Create a company
-            $newCompany = factory(Company::class)->create(['name' => $company]);
+    //         //Create a company
+    //         $newCompany = factory(Company::class)->create(['name' => $company]);
 
-            //Create relationship between Project and Company
-            $newProject->companies()->syncWithoutDetaching($newCompany);
+    //         //Create relationship between Project and Company
+    //         $newProject->companies()->syncWithoutDetaching($newCompany);
 
-            $project_company = $newProject->companies()->where('company_id', $newCompany->id)->first();  
+    //         $project_company = $newProject->companies()->where('company_id', $newCompany->id)->first();  
 
-            //Create Roles
-            foreach ($roles as $role) {
-                $adminRole = Role::create(['name' => $role . '/' . $newProject->id, 'project_id' => $newProject->id, 'nickname' => $role]);
-            }
+    //         //Create Roles
+    //         foreach ($roles as $role) {
+    //             $adminRole = Role::create(['name' => $role . '/' . $newProject->id, 'project_id' => $newProject->id, 'nickname' => $role]);
+    //         }
 
 
-            $newUserMaster = factory(UniversalPerson::class)->create(['identification' => '45960630','email' => 'jclavo@example.com',
-                                                'name' => 'Jose', 'lastname' => 'cleivor']);
+    //         $newUserMaster = factory(UniversalPerson::class)->create(['identification' => '45960630','email' => 'jclavo@example.com',
+    //                                             'name' => 'Jose', 'lastname' => 'cleivor']);
 
                                             
 
-            $newUser = factory(User::class)->create([
-                                                'login' => $newUserMaster->identification . $project_company->pivot->id,
-                                                'company_project_id' => $project_company->pivot->id,
-                                                'universal_person_id' =>  $newUserMaster->id,                     
-            ]);
+    //         $newUser = factory(User::class)->create([
+    //                                             'login' => $newUserMaster->identification . $project_company->pivot->id,
+    //                                             'company_project_id' => $project_company->pivot->id,
+    //                                             'universal_person_id' =>  $newUserMaster->id,                     
+    //         ]);
 
-            //Assign Role to user
-            $newUser->assignRole($adminRole->id);
+    //         //Assign Role to user
+    //         $newUser->assignRole($adminRole->id);
 
-            //Set guard
-            $adminRole->guard_name = $guard;
-            $adminRole->save();
+    //         //Set guard
+    //         $adminRole->guard_name = $guard;
+    //         $adminRole->save();
 
-            //Create modules for the project
-            foreach ($modules as $module) {
+    //         //Create modules for the project
+    //         foreach ($modules as $module) {
 
-                $newModule = $newProject->modules()->save(
-                    factory(Module::class)->make(['name' => $module->name,
-                                            'url' => $module->url, 
-                                            'visibled' =>  $module->activated])
-                );
+    //             $newModule = $newProject->modules()->save(
+    //                 factory(Module::class)->make(['name' => $module->name,
+    //                                         'url' => $module->url, 
+    //                                         'visibled' =>  $module->activated])
+    //             );
 
-                foreach ($module->resources as $resource) {
+    //             foreach ($module->resources as $resource) {
 
-                    $newResource = $newModule->resources()->save(
-                        factory(Resource::class)->make(['name' => $resource])
-                    );
+    //                 $newResource = $newModule->resources()->save(
+    //                     factory(Resource::class)->make(['name' => $resource])
+    //                 );
 
-                    $permissionNickname = strtolower($newResource->module->name . '/' . str_replace(" ","-", $newResource->name));
-                    $permissionName = $permissionNickname . '#' . $newResource->module->project_id;
+    //                 $permissionNickname = strtolower($newResource->module->name . '/' . str_replace(" ","-", $newResource->name));
+    //                 $permissionName = $permissionNickname . '#' . $newResource->module->project_id;
 
-                    $newPermission = Permission::create(['name' => $permissionName, 'guard_name' => $guard,
-                                                         'resource_id' => $newResource->id, 'nickname' => $permissionNickname]);
+    //                 $newPermission = Permission::create(['name' => $permissionName, 'guard_name' => $guard,
+    //                                                      'resource_id' => $newResource->id, 'nickname' => $permissionNickname]);
 
-                    $adminRole->givePermissionTo($newPermission->id);
+    //                 $adminRole->givePermissionTo($newPermission->id);
 
-                }
-            }
+    //             }
+    //         }
 
-            return $newProject;
-        });
-    }
+    //         return $newProject;
+    //     });
+    // }
 
 }
