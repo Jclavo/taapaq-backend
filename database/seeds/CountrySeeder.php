86456ea273ca4390f77e61e946a3c85021b0c88e
Jclavo <jclavotafur@gmail.com>
2020-11-15 21:38:54 -0300

    country seeder with translations

--- database/seeds/CountrySeeder.php
+++ database/seeds/CountrySeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Models\Country;
+use App\Models\Translation;
+use App\Models\TranslationDetail;
+use App\Utils\SystemModelUtil;
 use Illuminate\Database\Seeder;
 
 class CountrySeeder extends Seeder
@@ -12,12 +15,74 @@ class CountrySeeder extends Seeder
      */
     public function run()
     {
-        Country::updateOrCreate(['code' => '55'],
-                               ['name' => 'Brazil', 'timezone' => 'America/Sao_Paulo', 
-                                'currency' => 'BRL', 'locale' => 'pt', 'tax' => '20']);
+        // Country::updateOrCreate(['code' => '55'],
+        //                        ['name' => 'Brazil', 'timezone' => 'America/Sao_Paulo', 
+        //                         'currency' => 'BRL', 'locale' => 'pt', 'tax' => '20']);
 
-        Country::updateOrCreate(['code' => '51'],
-                               ['name' => 'Peru', 'timezone' => 'America/Lima',
-                                'currency' => 'PEN', 'locale' => 'es', 'tax' => '18']);
+
+
+        // Country::updateOrCreate(['code' => '51'],
+        //                        ['name' => 'Peru', 'timezone' => 'America/Lima',
+        //                         'currency' => 'PEN', 'locale' => 'es', 'tax' => '18']);
+
+
+        $countries = [
+            (object) array('code' => 55,'name' => 'Brazil', 'timezone' => 'America/Sao_Paulo', 
+                           'currency' => 'BRL', 'locale' => 'pt', 'tax' => '20',
+                            
+                        'translations' => 
+                            (object) array('key' => 'name',
+                            'details' => [
+                                    (object) array('value' => 'Brasil', 'locale' => 'es'),
+                                    (object) array('value' => 'Brasil', 'locale' => 'pt')]
+                            )
+                    ),
+            (object) array('code' => 51,'name' => 'Peru', 'timezone' => 'America/Lima',
+                           'currency' => 'PEN', 'locale' => 'es', 'tax' => '18',
+                     
+                        'translations' => 
+                            (object) array('key' => 'name',
+                            'details' => [
+                                    (object) array('value' => 'Perú', 'locale' => 'es'),
+                                    (object) array('value' => 'Peru', 'locale' => 'pt')]
+                            )
+                ),
+        ];
+
+        $newModel = SystemModelUtil::createFromProjectCode(env('PROJECT_TAAPAQ_CODE'),'COUNTRY');
+
+        foreach ($countries as $country) {
+            
+            $newCountry = Country::updateOrCreate(['code' => $country->code],
+                                    ['name' => $country->name, 'timezone' => $country->timezone, 
+                                    'currency' => $country->currency, 'locale' => $country->locale, 'tax' => $country->tax]);
+
+    
+            $newTranslation = Translation::updateOrCreate(['key' => $country->translations->key,
+                                                           'translationable_id' => $newCountry->id,
+                                                           'model_id' => $newModel->id,
+                                                        ]);
+
+            foreach ($country->translations->details as $detail) {
+            TranslationDetail::updateOrCreate(['translation_id' => $newTranslation->id, 'locale' => $detail->locale],
+                                            ['value' => $detail->value,]);
+            } 
+        }
+
+
+        // $newModel = SystemModelUtil::createFromProjectCode(env('PROJECT_TAAPAQ_CODE'),'SYSTEM');
+
+        // foreach ($this->translations as $translation) {
+
+        //     $translation->translationable_id < 1 ? $translation->translationable_id = 0 : null;
+
+        //     $newTranslation = Translation::updateOrCreate(['key' => $translation->key,
+        //                                                     'model_id' => $newModel->id,
+        //                                                     ]);
+        //     foreach ($translation->details as $detail) {
+        //             TranslationDetail::updateOrCreate(['translation_id' => $newTranslation->id, 'locale' => $detail->locale],
+        //                                                 ['value' => $detail->value,]);
+        //     } 
+        // }
     }
 }
